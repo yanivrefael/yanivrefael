68fc9e4028cd7071d1b631b75a9bec92f7a313d5
Yaniv Refael <yaniv.refael@unboundtech.com>
2022-03-14 11:39:56 +0200

    first commit

---
+++ test_addition.py
@@ -0,0 +1,9 @@
+#test_addition.py
+from src.calculator import add
+import pytest
+def test_add():
+    result = add(3, 4)
+    assert result == 7
+def test_add_string():
+    with pytest.raises(TypeError):
+        add("string", 4)
